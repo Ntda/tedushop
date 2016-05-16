9332603ec8594d488874800a01d54afeccb47d03
NguyenThanh <NguyenThanh@NOTEBOOK>
2016-05-16 08:49:24 +0700

    add table into repositories

---
+++ TeduShop.Data/Respositories/TagRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Respositories
+{
+    class TagRepository
+    {
+    }
+    public interface ITagRepository
+    {
+    }
+
+    public class SystemConfigRepository : RepositoryBase<Product>, ITagRepository
+    {
+        public SystemConfigRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
