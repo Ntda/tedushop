59006b6f11ee411e02fc18eb64beba0248ee0344
NguyenThanh <NguyenThanh@NOTEBOOK>
2016-05-17 17:19:39 +0700

    add dependency injection autofac into web

--- TeduShop.Service/ErrorService.cs
+++ TeduShop.Service/ErrorService.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Respositories;
+using TeduShop.Model.Models;
 
 namespace TeduShop.Service
 {
-    class ErrorService
+    public interface IErrorService
     {
+        Error Create(Error error);
+        void Save();
+    }
+    public class ErrorService : IErrorService
+    {
+        IErrorRepository _errorRepository;
+        IUnitOfWork _unitOfWork;
+        public ErrorService(IErrorRepository errorRepository, IUnitOfWork unitOfWork)
+        {
+            _errorRepository = errorRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public Error Create(Error error)
+        {
+            return _errorRepository.Add(error);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
     }
 }
