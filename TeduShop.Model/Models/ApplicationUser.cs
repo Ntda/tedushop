750f4edb6d5cd43321ef02672c93a9119ca22b38
NguyenThanh <NguyenThanh@NOTEBOOK>
2016-05-18 23:19:31 +0700

    add angulajs and bower

---
+++ TeduShop.Model/Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeduShop.Model.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [MaxLength(256)]
+        public string FullName { set; get; }
+        [MaxLength(256)]
+        public string Address { set; get; }
+        public DateTime? BirthDay { set; get; }
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
---
+++ TeduShop.Web/Api/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using TeduShop.Web.App_Start;
+
+namespace TeduShop.Web.Api
+{
+    [RoutePrefix("api/account")]
+    public class AccountController : ApiController
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public AccountController()
+        {
+        }
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.Current.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        [Route("login")]
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        
+        public async Task<HttpResponseMessage> Login(HttpRequestMessage request,string userName, string password, bool rememberMe)
+        {
+            if (!ModelState.IsValid)
+            {
+                return request.CreateErrorResponse(HttpStatusCode.BadRequest,ModelState);
+            }
+
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(userName, password, rememberMe, shouldLockout: false);
+            return request.CreateResponse(HttpStatusCode.OK, result);
+            //switch (result)
+            //{
+            //    case SignInStatus.Success:
+            //        return request.CreateResponse(HttpStatusCode.OK,result);
+            //    case SignInStatus.LockedOut:
+            //        return View("Lockout");
+            //    case SignInStatus.RequiresVerification:
+            //        return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+            //    case SignInStatus.Failure:
+            //    default:
+            //        ModelState.AddModelError("", "Invalid login attempt.");
+            //        return View(model);
+            //}
+        }
+    }
+}
