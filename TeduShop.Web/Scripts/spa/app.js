23a78fae126ccbbb0a81c3b58f00526ce25c1388
NguyenThanh <NguyenThanh@NOTEBOOK>
2016-05-19 21:13:56 +0700

    Service in angulars

--- TeduShop.Web/Scripts/spa/app.js
+++ TeduShop.Web/Scripts/spa/app.js
@@ -4,10 +4,17 @@ var myApp = angular.module('myModule', []);
 myApp.controller("schoolController", schoolController)
 myApp.controller("StudentController", StudentController);
 myApp.controller("TeacherController", TeacherController);
+myApp.service("Validator", Validator);
 myApp.$inject = ['$scope'];
+schoolController.$inject["$scope", "Validator"];
 
-function schoolController($scope) {
-    $scope.message="Message from school"
+function schoolController($scope, Validator) {
+    //$scope.message="Message from school"
+    $scope.checkNumber=function(){
+        Validator.checkNumber(2);
+    }
+    $scope.num = 1;
+   
 }
 //declare
 function StudentController(/*$rootScope*/$scope) {
@@ -17,4 +24,16 @@ function StudentController(/*$rootScope*/$scope) {
 
 function TeacherController($scope) {
     $scope.message = "this is my message from teacher";
+}
+
+function Validator($window) {
+    return {
+        checkNumber:checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2)
+            alert("So le");
+        else
+            alert("Solchan");
+    }
 }
