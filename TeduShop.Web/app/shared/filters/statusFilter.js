195249ce2ec0ed22e234fd9d98afdb4e05d4d3cd
NguyenThanh <NguyenThanh@NOTEBOOK>
2016-05-28 23:24:27 +0700

    add filter

---
+++ TeduShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,10 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input)
+                return 'Kích hoạt';
+            else
+                return 'khóa';
+        }
+    }); 
+})(angular.module('tedushop.common'));
